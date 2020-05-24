c64645569342251980742704b5e97c6f7a23dbd2
Aung Zin Phyo <aungzinphyo94@gmail.com>
2020-05-25 06:22:06 +0630

    AppBar Demo

---
+++ appBarDemo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+          appBar: AppBar(
+              title: const Text("AppBar Demo"),
+              leading: IconButton(
+                  icon: const Icon(Icons.backspace),
+                  tooltip: 'Back',
+                  onPressed: () {}),
+              actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.add_alert),
+                tooltip: 'Show Snackbar',
+                onPressed: () {})
+          ]),
+        body: const Center(
+          child: Text(
+            'This is the AppBar Demo',
+            style: TextStyle(fontSize: 28)
+          )
+        )
+      ),
+    );
+  }
+}
