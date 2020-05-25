91faee29dd8ac6e91c035738550d521cfc4dd8ac
Aung Zin Phyo <aungzinphyo94@gmail.com>
2020-05-25 06:44:53 +0630

    modified appBarDemo

--- appBarDemo.dart
+++ appBarDemo.dart
@@ -14,23 +14,21 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
               title: const Text("AppBar Demo"),
+              elevation: 8.0,
+              backgroundColor: Colors.red,
               leading: IconButton(
                   icon: const Icon(Icons.backspace),
                   tooltip: 'Back',
                   onPressed: () {}),
               actions: <Widget>[
-            IconButton(
-                icon: const Icon(Icons.add_alert),
-                tooltip: 'Show Snackbar',
-                onPressed: () {})
-          ]),
-        body: const Center(
-          child: Text(
-            'This is the AppBar Demo',
-            style: TextStyle(fontSize: 28)
-          )
-        )
-      ),
+                IconButton(
+                    icon: const Icon(Icons.add_alert),
+                    tooltip: 'Show Snackbar',
+                    onPressed: () {})
+              ]),
+          body: const Center(
+              child: Text('This is the AppBar Demo',
+                  style: TextStyle(fontSize: 28)))),
     );
   }
 }
